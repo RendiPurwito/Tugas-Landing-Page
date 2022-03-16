541cecee2cff5aaa6e60b129af7bc6ab1234f0d0
RendiPurwito <rendipurwito@gmail.com>
2022-03-16 13:08:47 +0700

    Final

--- Gaming Landing Page 1/js/script.js
+++ Gaming Landing Page 1/js/script.js
@@ -1,12 +1,20 @@
+// event pada saat link di klik
 $('.page-scroll').on('click', function(e) {
-
+    // mengambil isi href
     var tujuan = $(this).attr('href');
 
+    // menangkap elemen yang di klik
     var elemenTujuan = $(tujuan);
 
+    // jquery akan menjalankan animasi scroll ketika link di klik
+    // - 70 fungsi nya agar ketika di scroll tidak terlalu keatas
+    // 1500 fungsinya untuk mengatur delay waktu scroll
+    // easeInOutExpo jenis animasi scroll yang digunakan
     $('html , body').animate({
-    scrollTop: elemenTujuan.offset().top - 70
+    scrollTop: elemenTujuan.offset().top - 70 
     }, 1500, 'easeInOutExpo');
 
+    // mencegah terjadinya event bawaan href
+    // agar tidak terjadi reload ketika menjalankan perintah javascript nya
     e.preventDefault();
 });
