edd0200f1575dca63e7f1ba388bcb1fb3dfc7524
RendiPurwito <rendipurwito@gmail.com>
2022-03-28 13:47:56 +0700

    Kelompok 1 Finished

--- Kelompok 3/js/script.js
+++ Kelompok 3/js/script.js
@@ -1,3 +1,4 @@
+//! Guru
 var buttonLeft = document.querySelector(".left")
 
 var buttonRight = document.querySelector(".right")
@@ -11,4 +12,33 @@ buttonLeft.addEventListener("click", function(){
 buttonRight.addEventListener("click", function(){
     main3Content.scrollLeft += 250
 })
+// ! End Guru
 
+//! Prestasi Sekolah
+var buttonLeft2 = document.querySelector(".left2")
+var buttonRight2 = document.querySelector(".right2")
+var containerMain5 = document.querySelector(".container-main5")
+
+buttonLeft2.addEventListener("click", function(){
+    containerMain5.scrollLeft -= 250
+})
+
+buttonRight2.addEventListener("click", function(){
+    containerMain5.scrollLeft += 250
+})
+//! End Prestasi Sekolah
+
+
+//! Berita Terbaru
+var buttonLeft3 = document.querySelector(".left3")
+var buttonRight3 = document.querySelector(".right3")
+var bagInti = document.querySelector(".bag-inti")
+
+buttonLeft3.addEventListener("click", function(){
+    bagInti.scrollLeft -= 250
+})
+
+buttonRight3.addEventListener("click", function(){
+    bagInti.scrollLeft += 250
+})
+// ! End Berita Terbaru
